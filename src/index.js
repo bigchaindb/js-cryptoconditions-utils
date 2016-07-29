8504e94bb2861c3cfbdaaaa35af674877948f1ab
diminator <dimi.dejonghe@gmail.com>
2016-07-29 14:32:03 +0200

    license + index export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // Export the root-level general utilities without a namespace
-export filterByType from './filter_by_type';
-export parseEscrowData from './parse_escrow_data';
-export TypeIds from './type_ids';
+export { default as filterByType } from './filter_by_type';
+export { default as parseEscrowData } from './parse_escrow_data';
+export { default as TypeIds } from './type_ids';
